c56036d9e9531873515874eb39e84da1c9c1cb5c
Takeshi Kunimi <takc923@gmail.com>
2013-11-11 05:09:24 +0900

    you can go to tab if click notification panel

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+var tabMap = {};
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         var result = onMsgDispatcher[request.action](request.args, sender);
@@ -9,8 +11,15 @@ var onMsgDispatcher = {
     notice: function (args, sender) {
         chrome.windows.get(sender.tab.windowId, function(win) {
             if (! sender.tab.active || ! win.focused) {
-                chrome.notifications.create(args.id, args.options, function(){});
+                chrome.notifications.create(args.id, args.options, function(notificationId){
+                    tabMap[notificationId] = sender.tab;
+                });
             }
         });
     }
 };
+
+chrome.notifications.onClicked.addListener(function(notificationId){
+    chrome.tabs.update(tabMap[notificationId].id, {active: true});
+    chrome.windows.update(tabMap[notificationId].windowId, {focused: true});
+});
